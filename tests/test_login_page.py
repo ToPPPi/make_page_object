593acb5b1702ef01b505f5a4b78b406107b21f06
topppi <dmitriyqw1@gmail.com>
2025-01-14 04:12:29 +0300

    Allure reports_8

--- tests/test_login_page.py
+++ tests/test_login_page.py
@@ -1,9 +1,11 @@
 import pytest
 from pytest_check import check
+import allure
 from ..conftest import setup
 from ..pages.home_page import LoginPage
 
 @pytest.mark.usefixtures("setup")
+@allure.feature("Тесты для авторизации")
 class TestLogin:
 
     #Userdata
@@ -15,20 +17,28 @@ class TestLogin:
     def classSetup(self, setup):
         self.lp = LoginPage(self.driver)
 
-    def test_validLogin(self):
-        with check:
-            assert self.lp.verifyTitle() == "OrangeHRMM"
-        with check:
-            assert self.lp.verifyLoginSuccsessful(self.username, self.password_correct) == "Dashboard"
 
+    @allure.story("Проверка успешной авторизации")
+    @allure.title("Тест успешной авторизации")
+    def test_validLogin(self):
+        with allure.step("Проверка заголовка страницы"):
+            with check:
+                assert self.lp.verifyTitle() == "OrangeHRMM"
+        with allure.step("Проверка успешной авторизации"):
+            with check:
+                assert self.lp.verifyLoginSuccsessful(self.username, self.password_correct) == "Dashboard"
+
+
+    @allure.story("Проверка неуспешной авторизации")
+    @allure.title("Тест неуспешной авторизации")
     @pytest.mark.parametrize("username, password", [
         ("admin", "admin"),
         ("admin123", "admin123"),
         ("admin1", "admin1")])
     def test_invalidLogin(self, username, password):
-        with check:
-            assert self.lp.verifyLoginIncorrect(username, password) == "Invalid credentials"
-
+        with allure.step("Проверка неуспешной авторизации"):
+            with check:
+                assert self.lp.verifyLoginIncorrect(username, password) == "Invalid credentials"
 
 
 
